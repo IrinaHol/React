0076ccd80d500bfd5d7ddc51e8af92a0d145a5b9
IraHolovatska <irinagolovatska@i.ua>
2020-12-04 17:34:47 +0200

    Master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,1 @@
-import React, {Component} from 'react';
-import AllUsersComponents from "./components/all-users/AllUsersComponents";
 
-
-class App extends Component {
-  render() {
-    return (
-        <div>
-         <AllUsersComponents/>
-        </div>
-    );
-  }
-}
-
-export default App;
